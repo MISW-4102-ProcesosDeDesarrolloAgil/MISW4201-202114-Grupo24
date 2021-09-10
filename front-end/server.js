a2fe2f3b9fe8191f8d57c334ff49fa86ed3855c8
Juan Camilo Daza Medina <jc.dazam1@uniandes.edu.co>
2021-09-10 13:39:06 -0500

    Update server.js
    Update server.js

--- front-end/server.js
+++ front-end/server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/tutorial-canciones/src'));
+app.use(express.static(__dirname + '/dist/tutorial-canciones'));
 
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/tutorial-canciones/src/index.html'));
+res.sendFile(path.join(__dirname+'/dist/tutorial-canciones/index.html'));
 });
 
 app.listen(process.env.PORT || 5000);
