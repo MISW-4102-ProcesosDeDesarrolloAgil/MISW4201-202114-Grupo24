122b1e0b66398791bf55151fb5a856631407b532
Juan Camilo Daza <jc.dazam1@uniandes.edu.co>
2021-09-10 21:02:59 -0500

    Update ubicacion archivos

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname + '/front-end/tutorial-canciones/src'));
 
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/front-end/tutorial-canciones/src/index.html'));
+res.sendFile(path.join(__dirname + '/front-end/tutorial-canciones/src/index.html'));
 });
 
 app.listen(process.env.PORT || 5000);
