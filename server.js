8a75a4f5fd2a64697dc1388b354c971962cd2b99
Juan Camilo Daza <jc.dazam1@uniandes.edu.co>
2021-09-11 22:24:34 -0500

    cambio server.js

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist/tutorial-canciones'));
 
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname + '/src/index.html'));
-console.log("__dirname" +  __dirname)
+res.sendFile(path.join(__dirname+'/dist/tutorial-canciones/index.html'));
 });
 
 app.listen(process.env.PORT || 5000);
