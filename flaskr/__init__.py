4671abba4128bd6cfd28d55c6cc6b12567a9a106
Juan Camilo Daza Medina <jc.dazam1@uniandes.edu.co>
2021-09-20 21:43:08 -0500

    Update __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,10 +7,3 @@ def create_app(config_name):
     app.config['JWT_SECRET_KEY']='frase-secreta'
     app.config['PROPAGATE_EXCEPTIONS'] = True
     return app
-
-class TestingConfig(Config):
-    TESTING = True
-    SECRET_KEY="GGggjjjfk887856$%kk"
-    # Disable CSRF protection in the testing configuration
-    WTF_CSRF_ENABLED = False
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "test.sqlite")
